6f48d2cc18daf498dd5f02abd29a9bec5e918d0f
Patrick Bullock <bullockptravail@gmail.com>
2023-12-04 12:00:14 +0000

    Replace modulemontecarlos.py ; ajout de la possibilité de calculer P(T1 < X < T2) + graphes + tests

--- Module_MonteCarlo/modulemontecarlos.py
+++ Module_MonteCarlo/modulemontecarlos.py
@@ -1,64 +1,79 @@
-# -*- coding: utf-8 -*-
-
-import random
-
-def monte_carlo_integration(nbr_echant, fonction, intervalle):
-    a, b = intervalle
-    integral_estimate = 0
-
-    for _ in range(nbr_echant):
-        x = random.uniform(a, b)
-        y = fonction(x)
-
-        integral_estimate += y
-
-    intervalle_longueur = b - a
-    integral_estimate /= nbr_echant
-    integral_estimate *= intervalle_longueur
-
-    return integral_estimate
-
-def test():
-    # Définir différentes fonctions à tester
-    def fonction_a(x):
-        return x**2
-
-    def fonction_b(x):
-        return x**3 + 2*x + 1
-
-    def fonction_c(x):
-        return x**0.5
-
-    # Définir différents intervalles à tester
-    intervalle_a = (0, 1)
-    intervalle_b = (-1, 1)
-    intervalle_c = (1, 10)
-
-    # Nombre d'échantillons pour l'estimation de l'intégrale
-    nbr_echant = 3000000
-
-    # Tester chaque fonction sur chaque intervalle
-    fonctions_et_intervalles = [(fonction_a, intervalle_a), (fonction_b, intervalle_b), (fonction_c, intervalle_c)]
-
-    for fonction, intervalle in fonctions_et_intervalles:
-        resultat_estimation_integration = monte_carlo_integration(nbr_echant, fonction, intervalle)
-        nom_fonction = fonction.__name__
-        print(f"Estimation de l'intégrale de la fonction {nom_fonction} sur l'intervalle {intervalle} avec {nbr_echant} échantillons: {resultat_estimation_integration}")
-
-# Appeler la fonction de test
-test()
-
-###################
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-def monte_carlo_probabilite_normale(moyenne, ecart_type, t, nbr_echantillons=100000):
+def monte_carlo_probabilite_normale(moyenne, ecart_type, t1=None, t2=None, nbr_echantillons=300000):
+    # Vérifier si t1 > t2
+    if t1 is not None and t2 is not None and t1 >= t2:
+        print("Erreur: t1 doit être strictement inférieur à t2.")
+        return
+
     # Générer des échantillons aléatoires à partir de la distribution normale
     echantillons = np.random.normal(loc=moyenne, scale=ecart_type, size=nbr_echantillons)
 
-    # Calculer la probabilité P(X < t) avec la méthode de Monte Carlo
-    probabilite_estimee = np.sum(echantillons < t) / nbr_echantillons
+    # Calculer la probabilité P(t1 < X), P(X < t2), ou P(t1 < X < t2) avec la méthode de Monte Carlo
+    if t1 is not None and t2 is not None:
+        probabilite_estimee = np.sum((t1 < echantillons) & (echantillons < t2)) / nbr_echantillons
+        label = f'P({t1} < X < {t2})'
+    elif t1 is not None:
+        probabilite_estimee = np.sum(echantillons > t1) / nbr_echantillons
+        label = f'P(X > {t1})'
+    elif t2 is not None:
+        probabilite_estimee = np.sum(echantillons < t2) / nbr_echantillons
+        label = f'P(X < {t2})'
+    else:
+        print("Erreur: Fournissez au moins un des seuils t1 ou t2.")
+        return
+
+    # Afficher le résultat estimé
+    print(f'Probabilité estimée {label}: {probabilite_estimee:.4f}')
+
+
+######################################################
+
+
+def tester_monte_carlo():
+    # Cas 1-4: P(t1 < X)
+    monte_carlo_probabilite_normale(moyenne=0, ecart_type=1, t1=0)
+    monte_carlo_probabilite_normale(moyenne=2, ecart_type=5, t1=0.5)
+    monte_carlo_probabilite_normale(moyenne=-1, ecart_type=10, t1=-2.0)
+    monte_carlo_probabilite_normale(moyenne=3, ecart_type=15, t1=3.5)
+
+    # Cas 5-8: P(X < t2)
+    monte_carlo_probabilite_normale(moyenne=0, ecart_type=1, t2=1.5)
+    monte_carlo_probabilite_normale(moyenne=2, ecart_type=5, t2=3.0)
+    monte_carlo_probabilite_normale(moyenne=-1, ecart_type=10, t2=0)
+    monte_carlo_probabilite_normale(moyenne=3, ecart_type=15, t2=2.5)
+
+    # Cas 9-12: P(t1 < X < t2)
+    monte_carlo_probabilite_normale(moyenne=0, ecart_type=1, t1=1.0, t2=2.0)
+    monte_carlo_probabilite_normale(moyenne=2, ecart_type=5, t1=0.5, t2=3.5)
+    monte_carlo_probabilite_normale(moyenne=-1, ecart_type=10, t1=2, t2=2)
+    monte_carlo_probabilite_normale(moyenne=3, ecart_type=15)
+
+# Tester tous les cas
+# Les resultat ne sont jamais les mêmes, c'est dû à la fonction random
+tester_monte_carlo()
+
+
+#####################################################
+
+
+def monte_carlo_probabilite_normale_graphe(moyenne, ecart_type, t1=None, t2=None, nbr_echantillons=300000):
+    # Générer des échantillons aléatoires à partir de la distribution normale
+    echantillons = np.random.normal(loc=moyenne, scale=ecart_type, size=nbr_echantillons)
+
+    # Calculer la probabilité P(t1 < X), P(X < t2), ou P(t1 < X < t2) avec la méthode de Monte Carlo
+    if t1 is not None and t2 is not None:
+        probabilite_estimee = np.sum((t1 < echantillons) & (echantillons < t2)) / nbr_echantillons
+        label = f'P({t1} < X < {t2})'
+    elif t1 is not None:
+        probabilite_estimee = np.sum(echantillons > t1) / nbr_echantillons
+        label = f'P(X > {t1})'
+    elif t2 is not None:
+        probabilite_estimee = np.sum(echantillons < t2) / nbr_echantillons
+        label = f'P(X < {t2})'
+    else:
+        raise ValueError("Au moins un des seuils t1 ou t2 doit être fourni.")
 
     # Affichage de la courbe de densité de probabilité normale
     x_values = np.linspace(moyenne - 4 * ecart_type, moyenne + 4 * ecart_type, 1000)
@@ -66,98 +81,28 @@ def monte_carlo_probabilite_normale(moyenne, ecart_type, t, nbr_echantillons=100
     plt.plot(x_values, pdf_values, color='red', linewidth=2, label='Densité de probabilité normale')
 
     # Mise en forme du graphique
-    plt.fill_between(x_values, pdf_values, where=(x_values < t), color='blue', alpha=0.3, label=f'P(X < {t})')
-    plt.title('Estimation de la probabilité P(X < t) avec la méthode de Monte Carlo')
+    if t1 is not None and t2 is not None:
+        plt.fill_between(x_values, pdf_values, where=((x_values > t1) & (x_values < t2)), color='blue', alpha=0.3, label=label)
+    elif t1 is not None:
+        plt.fill_between(x_values, pdf_values, where=(x_values > t1), color='blue', alpha=0.3, label=label)
+    elif t2 is not None:
+        plt.fill_between(x_values, pdf_values, where=(x_values < t2), color='blue', alpha=0.3, label=label)
+
+    plt.title('Estimation de la probabilité avec la méthode de Monte Carlo')
     plt.xlabel('Valeurs')
     plt.ylabel('Densité de probabilité')
     plt.legend()
     plt.show()
+
     # Afficher le résultat estimé
-    print(f'Probabilité estimée P(X < {t}): {probabilite_estimee:.4f}')
+    print(f'Probabilité estimée {label}: {probabilite_estimee:.4f}')
 
-monte_carlo_probabilite_normale(moyenne=0, ecart_type=1, t=1.2)
+# Exemple d'utilisation avec P(t1 < X)
+monte_carlo_probabilite_normale_graphe(moyenne=0, ecart_type=1, t1=1.2)
 
-##################
+# Exemple d'utilisation avec P(X < t2)
+monte_carlo_probabilite_normale_graphe(moyenne=0, ecart_type=1, t2=1.5)
 
-def monte_carlo_probabilite_normale_sans_graphe(moyenne, ecart_type, t, nbr_echantillons=300000):
-    # Générer des échantillons aléatoires à partir de la distribution normale
-    echantillons = np.random.normal(loc=moyenne, scale=ecart_type, size=nbr_echantillons)
-
-    # Calculer la probabilité P(X < t) avec la méthode de Monte Carlo
-    probabilite_estimee = np.sum(echantillons < t) / nbr_echantillons
-
-    print(f'Probabilité estimée P(X < {t}): {probabilite_estimee:.4f}')
-
-def test_monte_carlo():
-  monte_carlo_probabilite_normale_sans_graphe(moyenne=0, ecart_type=1, t=0)
-  monte_carlo_probabilite_normale_sans_graphe(moyenne=0, ecart_type=1, t=1.2)
-  monte_carlo_probabilite_normale_sans_graphe(moyenne=2, ecart_type=4, t=3.5)
-  monte_carlo_probabilite_normale_sans_graphe(moyenne=-1, ecart_type=0.5, t=-1.4)
-  monte_carlo_probabilite_normale_sans_graphe(moyenne=6, ecart_type=20, t=33)
-  monte_carlo_probabilite_normale_sans_graphe(moyenne=0, ecart_type=50, t=75)
-
-test_monte_carlo()
-
-######## Exemple d'utilisation plus complexe #########
-
-"""### Recouvrement de courbes et méthode contrainte-résistance
-
-Exemple :
-Un ingénieur conçoit des câbles électriques pour une application. La contrainte est la charge électrique maximale qu'un câble doit supporter, et la résistance est la capacité du câble à supporter cette charge.
-
-Supposons que la contrainte électrique (S) suit une distribution normale avec une moyenne de 10 Ampères et un écart-type de 2 Ampères. La résistance du câble (R) suit également une distribution normale avec une moyenne de 12 Ampères et un écart-type de 1 Ampère.
-
-L'ingénieur souhaite savoir quelle est la probabilité que la résistance du câble soit supérieure à la contrainte électrique (P(R>S)) pour garantir que le câble ne tombe pas en panne dans 99% des cas.
-
-Voici comment cela pourrait être implémenté en utilisant la méthode de Monte-Carlo avec la probabilité complémentaire :
-
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
-
-def contrainte_resistance_monte_carlo(S, R):
-    cas_reussis = np.sum(R > S)
-    probabilite_complementaire = cas_reussis / len(S)
-    return probabilite_complementaire
-
-# Nombre de simulations Monte-Carlo
-nbr_simulations = 100000
-
-# Générer des tirages aléatoires pour les variables aléatoires S et R
-S = np.random.normal(loc=10, scale=2, size=nbr_simulations)  # Contrainte électrique S (distribution normale)
-R = np.random.normal(loc=15, scale=1, size=nbr_simulations)  # Résistance du câble R (distribution normale)
-
-# Appliquer la méthode de Monte-Carlo pour déterminer la probabilité complémentaire P(R > S)
-probabilite_complementaire = contrainte_resistance_monte_carlo(S, R)
-
-# Affichage des courbes de probabilités normales
-x_values = np.linspace(0, 20, 1000)
-plt.plot(x_values, norm.pdf(x_values, loc=10, scale=2), label="Contrainte électrique S")
-plt.plot(x_values, norm.pdf(x_values, loc=15, scale=1), label="Résistance du câble R")
-
-# Remplir la zone sous l'intersection des deux courbes avec une couleur spécifique
-plt.fill_between(x_values, norm.pdf(x_values, loc=10, scale=2), norm.pdf(x_values, loc=15, scale=1), color='gray', alpha=0.5, label='Zone Intersection')
-
-# Mise en forme du graphique
-plt.xlabel("Charge électrique (Ampères)")
-plt.ylabel("Densité de probabilité")
-plt.title("Contrainte-Résistance pour la conception de câbles électriques")
-plt.legend()
-plt.show()
-
-# Affichage du résultat
-print(f"Probabilité complémentaire P(R > S) : {probabilite_complementaire}")
-
-"""D'après les résultats de la simulation Monte-Carlo, la probabilité complémentaire que la résistance (R) du câble soit supérieure à la contrainte électrique (S) est estimée à environ 98,72%. En d'autres termes, dans la plupart des cas simulés, la résistance du câble est suffisamment élevée pour supporter la contrainte électrique.
-
-Cette information est utile pour l'ingénieur, car une probabilité complémentaire élevée suggère une conception robuste du câble, ce qui signifie qu'il est probable que la résistance du câble soit plus grande que la contrainte électrique. Cela contribue à la fiabilité du câble dans des situations réelles où la charge électrique peut varier.
-
-En général, plus la probabilité complémentaire P(R>S) est élevée, plus la marge de sécurité est grande, ce qui est souvent souhaitable dans la conception de systèmes critiques.
-"""
-
-
-
-
+# Exemple d'utilisation avec P(t1 < X < t2)
+monte_carlo_probabilite_normale_graphe(moyenne=0, ecart_type=1, t1=-1.0, t2=1.5)
 
