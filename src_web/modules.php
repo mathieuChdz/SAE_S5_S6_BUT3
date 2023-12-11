3355e7762888d5e9a1585f12a6f9323383b13cbb
unknown <chedozeaumathieu@gmail.com>
2023-12-11 11:38:49 +0100

    debut responsive

--- src_web/modules.php
+++ src_web/modules.php
@@ -38,9 +38,9 @@
                     <h2>Modules disponibles</h2>
                 </div>
                 <div class="main-explication-texte">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.</p>
                 </div>
             </div>
 
